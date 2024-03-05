3ce6e4c00775daf044d0dd7c0eb3bf6b0fc76a17
hao.thilinh <hao.thilinh@amela.vn>
2024-03-05 08:53:57 +0700

    base code

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="home-one">
+        <div class="swiper home-swiper-container">
+            <div class="swiper-wrapper home-swiper-wrapper">
+                <div class="swiper-slide home-swiper-slide">
+                    <img src="/assets/images/home/image-slide-1.jpg" alt="image">
+                    <p class="font-zen-maru">春の新規免許取得キャンペーン実施中</p>
+                </div>
+                <div class="swiper-slide home-swiper-slide">
+                    <img src="assets/images/home/image-slide-2.jpg" alt="image">
+                    <p class="font-zen-maru">ペーパードライバーキャンペーンも実施中</p>
+                </div>
+                <div class="swiper-slide home-swiper-slide">
+                    <img src="assets/images/home/image-slide-3.jpg" alt="image">
+                    <p class="font-zen-maru">オンライン学科込みのプラン</p>
+                </div>
+                <div class="swiper-slide home-swiper-slide">
+                    <img src="assets/images/home/image-slide-4.jpg" alt="image">
+                    <p class="font-zen-maru">オンライン無料相談会実施中</p>
+                </div>
+            </div>
+
+            <div class="swiper-pagination home-swiper-pagination"></div>
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+        </div>
+        <div class="home-one-qr">
+            <div class="home-one-qr-text">
+                <span>LINEからも</span>
+                <span>予約可能</span>
+            </div>
+            <img src="assets/images/home/section-one-qr.svg" alt="qr-code">
+        </div>
+    </div>
+</body>
+</html>
